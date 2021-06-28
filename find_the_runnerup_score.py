a5bae593c960b74ae5ece0a42d5d7a51a110931a
haseul <jeroldcamacho.res@gmail.com>
2021-06-28 18:54:35 +0800

    find the runner up score

--- find_the_runnerup_score.py
+++ find_the_runnerup_score.py
@@ -1,9 +1,8 @@
 if __name__ == '__main__':
-        n = int(input())
-        arr = map(int, input().split())
+    n = int(input())
+    arr = map(int, input().split())
 
-        arr_ = list(arr)
+    arr_ = list(arr)
+    arr_1 = [val for val in arr_ if val != max(arr_)]
 
-        arr_1 = [val for val in arr_ if val != max(arr_)]
-
-        print(max(arr_1))
+    print(max(arr_1))
